ea0fce05f120a979d4fbe574f65244f9955da9b0
Muhardiansyah <70386390+muhardiansyah15@users.noreply.github.com>
2023-08-03 00:12:41 +0700

    Update spotifyAuthConfig.jsx

--- src/components/spotifyAuthConfig.jsx
+++ src/components/spotifyAuthConfig.jsx
@@ -2,7 +2,7 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "fffd485243824e48867fb8a4a454988c";
-export const redirectUri = "https://muhardiansyah15.github.io/my-spotify-stats/redirect";
+export const redirectUri = "https://muhardiansyah15.github.io/my-spotify-stats";
 export const scopes = [
     "user-top-read",
     "user-read-currently-playing",
